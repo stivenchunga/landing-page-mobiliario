9fe9f45c27dcdabb115d9dcadc443829fad630f2
stivenchunga <sschungav@unipacifico.edu.co>
2022-10-18 01:30:31 -0500

    se termina landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  productos : any = [
+    {nombre: "sillas", precio: 1000 },
+    {nombre: "Carpas", precio: 35000 },
+    {nombre: "Mesas", precio: 15000 },
+    {nombre: "Maneteles", precio: 10000 },
+    {nombre: "Velas", precio: 3000 },
+    {nombre: "Cilindros", precio: 70000 },
+    {nombre: "Platos", precio: 3000 },
+    {nombre: "Cucharas", precio: 500 },
+  ];
   constructor() { }
 
   ngOnInit(): void {
